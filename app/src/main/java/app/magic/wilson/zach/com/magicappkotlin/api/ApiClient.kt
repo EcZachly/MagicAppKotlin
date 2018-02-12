97babd4f700bdeaa6dd1c68d0d63314fde33ad36
Alex Blass <ajblack.000@hotmail.com>
2018-02-11 17:53:32 -0800

    Docs: Clean up code and documentation
    * Update comments and descriptions of classes
    * Remove log statements
    * Remove unused import statements
    * Rename variables for clarity

--- app/src/main/java/app/magic/wilson/zach/com/magicappkotlin/api/ApiClient.kt
+++ app/src/main/java/app/magic/wilson/zach/com/magicappkotlin/api/ApiClient.kt
@@ -1,19 +1,14 @@
 package app.magic.wilson.zach.com.magicappkotlin.api
 
-import android.util.Log
-import app.magic.wilson.zach.com.magicappkotlin.models.Card
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.core.Request
-import com.github.kittinunf.fuel.core.Response
-import com.github.kittinunf.result.Result
-import java.sql.ResultSet
 
 /**
- * Created by Zach on 2/10/18.
+ * The API Client to get card data from the server.
  */
 
 
 fun getCards(color: String?, rarity: String?) : Request {
+    // TODO("Make this more generic to enable other languages")
     return Fuel.get("/mtg/cards", listOf("language" to "en", "colors" to "red"))
 }
